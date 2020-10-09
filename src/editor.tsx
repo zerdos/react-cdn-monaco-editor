a0a9cd84d6ea0c22c60f15da3b22866d3f877aa3
Zoltan Erdos <zoltan.erdos@me.com>
2020-10-09 20:21:13 +0100

    jsx

--- src/editor.tsx
+++ src/editor.tsx
@@ -14,7 +14,6 @@ type MonacoEditorProps = {
 
 type ReactType = typeof React;
 
-
 const getMonacoEditor = (React: ReactType) => {
   // const ReactTypeJs = DTSGen.generateIdentifierDeclarationFile("React", React);
   // const dts = generateModuleDeclarationFile(React, "react");
@@ -85,26 +84,45 @@ const startMonaco = ({
 const startMonaco = async ({version, element, value, language}) => {
   const vsPath = 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/${version}/min/vs';
   await loadScript('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/${version}/min/vs/loader.min.js');
+  await loadScript('https://unpkg.com/react-cdn-monaco-editor@1.1.1/dts-gen.bundle.js');
 
   require.config({ paths: { 'vs': vsPath } });
 
   return new Promise(function(resolve, reject){
   
     require(["vs/editor/editor.main"], function () {
+
+
+
       const editor = monaco.editor.create(document.getElementById("container"), {
+        model: monaco.editor.createModel(\`${value}\`, "typescript", monaco.Uri.parse("file:///main.tsx")),
         value: \`${value}\`,
         language: \`${language}\`,
         theme: 'vs-dark'
       });
 
+
+      monaco.editor.createModel( DTSGen.generateIdentifierDeclarationFile("React", React) + \`
+      declare module "react" {
+       export = React;
+       export as namespace React;
+      } \`, "typescript", monaco.Uri.parse("file:///index.d.ts"));
+
+
+
         console.log(monaco.languages.typescript)
   monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+   
     target: monaco.languages.typescript.ScriptTarget.ES2016,
     allowNonTsExtensions: true,
-    jsx: monaco.languages.typescript.React,
     moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
     module: monaco.languages.typescript.ModuleKind.CommonJS,
     noEmit: true,
+    typeRoots: ["node_modules/@types"],
+    jsx: monaco.languages.typescript.JsxEmit.React,
+    jsxFactory: 'React.createElement',
+
+    esModuleInterop: true
    });
 
    monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
@@ -112,7 +130,6 @@ const startMonaco = async ({version, element, value, language}) => {
     noSyntaxValidation: true,
   });
 
-   monaco.languages.typescript.typescriptDefaults.addExtraLib(\`declare module "react"\`)
   
       editor.onDidChangeModelContent((event)=>onChange(editor.getValue()))
       resolve(editor);
