38dea6637a378d51ea466b1805ee6bb42f7e8a81
Zoltan Erdos <zoltan.erdos@me.com>
2020-10-18 00:02:56 +0100

    added desc and example

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 
 
-[DEMO](https://zerdos.github.io/react-cdn-monaco-editor/) 
+[DEMO - normal html file, without react or any build tool ](https://zerdos.github.io/react-cdn-monaco-editor/) 
+
+[DEMO - with react create app ](https://zerdos.github.io/react-cdn-monaco-editor/) 
 
 
 <h1 align="center">react-cdn-monaco-editor</h1>
 
 ##examples added
+
+Works with typescript, and it knows jsx as well.
+
+It loads all the necessary files from CDN at runtime, so it wont add extra 10 minutes to your build pipeline.
+
 
 [Monaco Editor](https://github.com/Microsoft/monaco-editor) for React.
---
+++ example/build/precache-manifest.f1392ebd7e2cfc923692a1a49a40c2d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d61d1f8c55e8f2852c2189919126477",
+    "url": "/react-cdn-monaco-editor/example/build/index.html"
+  },
+  {
+    "revision": "f25a89804c6d915559ff",
+    "url": "/react-cdn-monaco-editor/example/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f49737131b0ecd360a4",
+    "url": "/react-cdn-monaco-editor/example/build/static/js/2.19337a2e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-cdn-monaco-editor/example/build/static/js/2.19337a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25a89804c6d915559ff",
+    "url": "/react-cdn-monaco-editor/example/build/static/js/main.1c27836e.chunk.js"
+  },
+  {
+    "revision": "d8e59220801fb2e4cf43",
+    "url": "/react-cdn-monaco-editor/example/build/static/js/runtime-main.6c9051da.js"
+  }
+]);
