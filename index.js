b5f3dc63a5d74677c1bd13e741e3945428205661
Zoltan Erdos <zoltan.erdos@me.com>
2020-10-18 00:32:19 +0100

    ejj

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export async function startMonaco({ onChange, code }) {
-    const remoteFile = await fetch(`https://unpkg.com/react-cdn-monaco-editor@1.7.5/editor.js`);
+    const remoteFile = await fetch(`https://unpkg.com/react-cdn-monaco-editor@1.8.0/editor.js`);
     const remoteAsText = await remoteFile.text();
     const replaced = remoteAsText.replaceAll("export", "");
     const stM = new Function(`return startMonaco; 
