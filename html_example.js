4ed48d3918b214a128c2583fcc226b6c605a2626
Zoltan Erdos <zoltan.erdos@me.com>
2020-10-19 21:43:09 +0100

    html example

--- html_example.js
+++ html_example.js
@@ -1,17 +1,30 @@
 export const example = `<!DOCTYPE html>
+<html>
+
 <head>
   <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-  <meta content="utf-8" http-equiv="encoding">
-  <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
-  <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+  <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
+  <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+  <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+  <title>Hello Example</title>
   <style>
     body {
       margin: auto;
-      background-color: cadetblue;
-      overflow: hidden;
+      overflow-y: auto;
     }
-</style>
+  </style>
 </head>
+
 <body>
-  <h1>hello world!</h1>
-</body>`
+  <div id="output"></div>
+
+  <script type="text/jsx">
+
+    const Hello = ({name}) => <h1>Hello {name}</h1>;
+    ReactDOM.render(<Hello name="React"/>, document.getElementById("output"))
+
+</script>
+</body>
+<html>
+
+`
