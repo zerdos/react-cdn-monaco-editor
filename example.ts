63eccbdbd9140e22c2252a57df2c0a3376a04f1e
Zoltan Erdos <zoltan.erdos@me.com>
2020-10-23 06:24:12 +0100

    prepublish-2.3.1

---
+++ example.ts
@@ -0,0 +1,205 @@
+import { diff, diff_cleanupMerge } from "./diff.js";
+// import interact from "./node_modules/@interactjs/interactjs/index.js"
+
+//@ts-ignore
+export const run = async (React, ReactDOM, Babel, startMonaco) => {
+  const searchRegExp = /import/gi;
+  const replaceWith = "///";
+
+  const transpileCode = (code: string) =>
+    Babel.transform(code, {
+      plugins: [],
+      presets: ["react", ["typescript", { isTSX: true, allExtensions: true }]],
+    }).code.replace(searchRegExp, replaceWith);
+
+  const restartCode = (transpileCode: string) => {
+    const restart = new Function(
+      "transpileCode",
+      `return function(){ ${transpileCode} }`,
+    )();
+    restart();
+  };
+
+  let keystrokeTillNoError = 0;
+  let latestCode = "";
+  let errorReported = "";
+  let busy = 0;
+  let latestGoodCode = "";
+  let latestBadCode = "";
+
+  (async () => {
+    const example = getExampleCode();
+    latestGoodCode = example;
+    latestBadCode = example;
+    const editor = await startMonaco({
+      language: "typescript",
+      code: example,
+      onChange: (code: string) => {
+        latestCode = code;
+        console.log(code);
+
+        const runner = async (cd: string) => {
+          if (busy === 1) return;
+
+          busy = 1;
+          const err = await getErrors(editor);
+          ///@ts-ignore
+          const errorDiv = document.getElementById("error");
+          try {
+            busy = 0;
+
+            if (cd !== latestCode) return;
+            if (err && err.length) {
+              if (latestCode != cd) return;
+              if (errorReported === cd) return;
+
+              const slices = diff(latestGoodCode, cd);
+              console.log(slices);
+
+              if (slices.length <= 3) {
+                latestBadCode = cd;
+                ///@ts-ignore
+                window["monaco"].editor.setTheme("hc-black");
+                return;
+              }
+
+              const oldSlices = diff(latestBadCode, cd);
+
+              const unMerge = oldSlices.filter((o) => o[0] !== 0);
+
+              let filtered = slices.filter((t) =>
+                t[0] === 0 || t[1] === unMerge[0][1]
+              );
+
+              if (filtered.length > 4) {
+                filtered = filtered.filter((t) => t[0] === 0);
+              }
+
+              diff_cleanupMerge(filtered, false);
+
+              let newStr = "";
+              let offset = 0;
+              filtered.map((t) => {
+                newStr = newStr + t[1];
+                if (t[0] !== 0) {
+                  offset = newStr.length;
+                }
+              });
+
+              busy = 0;
+
+              if (newStr !== cd) {
+                editor.setValue(newStr);
+                const model = editor.getModel("file:///Main.tsx");
+
+                const position = model.getPositionAt(offset);
+                //  mosel.getC
+
+                const validPos = model.validatePosition(position);
+                editor.setPosition(validPos);
+
+                // model.modifyPosition(position)
+                return;
+              }
+              const errors = err.map((x) => x.messageText).join("<br />");
+
+              //@ts-ignore
+              errorDiv.innerHTML = errors;
+
+              errorDiv.style.display = "block";
+              errorReported = cd;
+
+              //@ts-ignore
+              window["monaco"].editor.setTheme("vs-light");
+              setTimeout(() => {
+                //@ts-ignore
+                window["monaco"].editor.setTheme("hc-black");
+              }, keystrokeTillNoError++);
+
+              return;
+            }
+            latestGoodCode = cd;
+
+            errorDiv!.style!.display = "none";
+            //@ts-ignore
+            window["monaco"].editor.setTheme("vs-dark");
+            keystrokeTillNoError = 0;
+
+            busy = 0;
+            restartCode(transpileCode(cd));
+          } catch (err) {
+            busy = 0;
+            if (cd !== latestCode) return;
+
+            ///@ts-ignore
+            window["monaco"].editor.setTheme("vs-light");
+            setTimeout(() => {
+              ///@ts-ignore
+              window["monaco"].editor.setTheme("hc-black");
+            }, 10);
+            console.error(err);
+          }
+        };
+        if (!busy) runner(latestCode);
+        else {
+          const myCode = code;
+          const cl = setInterval(() => {
+            if (myCode !== latestCode || !busy) {
+              clearInterval(cl);
+            }
+
+            if (!busy) runner(latestCode);
+          }, 100);
+        }
+      },
+    });
+    //
+  })();
+
+  restartCode(transpileCode(getExampleCode()));
+  // dragElement(document.getElementById("root"));
+
+  async function getErrors(editor: any) {
+    const model = editor.getModel("file:///main.tsx");
+
+    //@ts-ignore
+    const tsWorker = await window["monaco"].languages.typescript
+      .getTypeScriptWorker();
+    const modelUri = model?.uri;
+    if (!modelUri) return;
+
+    const diag = await (await tsWorker(modelUri)).getSemanticDiagnostics(
+      "file:///main.tsx",
+    );
+    const comp = await (await tsWorker(modelUri))
+      .getCompilerOptionsDiagnostics("file:///main.tsx");
+    const syntax = await (await tsWorker(modelUri)).getSyntacticDiagnostics(
+      "file:///main.tsx",
+    );
+
+    return [...diag, ...comp, ...syntax];
+  }
+
+  function getExampleCode() {
+    return `import * as React from "react";
+import ReactDOM from "react-dom";
+
+const Counter = (
+{ initial = 0 }
+) => {
+const [clicks, setClicks] = React.useState(initial);
+
+return <div>
+    <p>Clicks: <strong>{clicks}</strong></p>
+    <button onClick={() => setClicks(clicks + 1)}>+</button>
+    <button onClick={() => setClicks(clicks - 1)}>-</button>
+</div>;
+};
+
+ReactDOM.render(
+<Counter initial={0} />,
+document.getElementById("root")
+);
+`;
+  }
+};
--- example/build/precache-manifest.89cc6cc98f9cbe3449643141bb88df99.js
+++ example/build/precache-manifest.89cc6cc98f9cbe3449643141bb88df99.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4fbf6550986e97aa1a9d7b383837f0c2",
+    "revision": "d08f32cdbd3aee0b9324e419bfc1ae9f",
     "url": "/react-cdn-monaco-editor/example/build/index.html"
   },
   {
-    "revision": "e566dee05fb2b25d0a57",
+    "revision": "916e2c402c58cf27afff",
     "url": "/react-cdn-monaco-editor/example/build/static/css/main.88fbda7c.chunk.css"
   },
   {
-    "revision": "20c1e8ba5b268dd67fbb",
-    "url": "/react-cdn-monaco-editor/example/build/static/js/2.50b656c6.chunk.js"
+    "revision": "856e3c446c7744e6be9a",
+    "url": "/react-cdn-monaco-editor/example/build/static/js/2.ec2c3e02.chunk.js"
   },
   {
     "revision": "f5fc2afae58c4ffc30c6148864765d4b",
-    "url": "/react-cdn-monaco-editor/example/build/static/js/2.50b656c6.chunk.js.LICENSE.txt"
+    "url": "/react-cdn-monaco-editor/example/build/static/js/2.ec2c3e02.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e566dee05fb2b25d0a57",
-    "url": "/react-cdn-monaco-editor/example/build/static/js/main.437f67b7.chunk.js"
+    "revision": "916e2c402c58cf27afff",
+    "url": "/react-cdn-monaco-editor/example/build/static/js/main.868a7c3e.chunk.js"
   },
   {
     "revision": "d8e59220801fb2e4cf43",
