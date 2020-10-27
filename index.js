86661fef19a4ce53e1e36c54e43a9f5fcbd27581
Zoltan Erdos <zoltan.erdos@me.com>
2020-10-27 16:50:09 +0000

    save

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 
-const importSpecificVersion = (version) =>  import (`https://unpkg.com/react-cdn-monaco-editor@${version}/editor.js`);
+const importSpecificVersion = (version) =>  import (`https://unpkg.com/react-cdn-monaco-editor@${version}/lib/editor.js`);
 
 export const startMonaco = async(...args) => {
 let version;
     if (typeof window !== undefined) {
         try{
-        const sp= await fetch("./package.json");
-        const data = await sp.json();
-        version = data.version;
+            const sp= await fetch("./package.json");
+            const data = await sp.json();
+            version = data.version;
         } catch(e) {
             version="latest"
         }
-        version="latest";
 
         const {startMonaco} = await importSpecificVersion(version);
         return startMonaco(...args);
